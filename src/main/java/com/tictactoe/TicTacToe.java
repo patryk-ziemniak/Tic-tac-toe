b95897ca4f097826372f21ea52016c3967220ad5
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-07-10 00:48:56 +0200

    Temporary UI made

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -1,30 +1,57 @@
 package com.tictactoe;
 
-public class TicTacToe {
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class TicTacToe extends Application {
+
+    private Image background = new Image("file:src/main/resources/background.jpg");
+    private Image SymbolO = new Image("file:src/main/resources/Symbol-O.jpg");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setBackground(background);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+
+        ColumnConstraints col1 = new ColumnConstraints(114);
+        ColumnConstraints col2 = new ColumnConstraints(114);
+        ColumnConstraints col3 = new ColumnConstraints(114);
+        ColumnConstraints col4 = new ColumnConstraints(308);
+        RowConstraints row1 = new RowConstraints(114);
+        RowConstraints row2 = new RowConstraints(114);
+        RowConstraints row3 = new RowConstraints(114);
+        gridPane.getColumnConstraints().addAll(col1, col2, col3, col4);
+        gridPane.getRowConstraints().addAll(row1,row2,row3);
+        gridPane.setGridLinesVisible(true);                //linie oddzielajace kolumny i rzedy
+
+        ImageView Symbol11 = new ImageView(SymbolO);
+        gridPane.add(Symbol11, 1, 1);
+        GridPane.setHalignment(Symbol11, HPos.LEFT);
+        GridPane.setValignment(Symbol11, VPos.TOP);
+
+
+        Scene scene = new Scene(gridPane, 700, 392, Color.BLACK);
+
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        Symbol playerSymbol = gameController.getPlayerSymbol();
-        Symbol computerSymbol = gameController.getComputerSymbol();
-        //gameController.newGame(); Loop 'for' for displaying status of every field; <-0 Do that when player clicks on button "New Game"
-
-        while (gameController.checkGameStatus() == GameStatus.PROCESSING) {
-            //Display who's move
-            if(gameController.getWhoseMove() == computerSymbol) {
-                gameController.doComputerMove();
-                gameController.setWhoseMove(playerSymbol);
-            } else {
-                gameController.setSymbol(playerSymbol, 0,0); //Do that on player click on specific field(X,Y)
-                gameController.setWhoseMove(computerSymbol);
-            }
-            //Loop 'for' for displaying status of every field (X/O/Empty)
-        }
-        if(gameController.checkGameStatus() == GameStatus.DRAW) {
-            //Show text "DRAW" on game board
-        } else if (gameController.checkGameStatus() == GameStatus.LOSE) {
-            //Show text "YOU LOSE" on game board
-        } else if (gameController.checkGameStatus() == GameStatus.WIN) {
-            //Show text "YOU WIN" on game board
-        }
+        launch(args);
     }
 }
