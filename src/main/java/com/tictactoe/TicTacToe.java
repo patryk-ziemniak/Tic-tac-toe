8fd0954c5fe7cda9ca2c802707c76e7f92742464
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-07-12 00:53:19 +0200

    Temp commit

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -19,6 +19,14 @@ public class TicTacToe extends Application {
     private final Image symbolO = new Image("file:src/main/resources/symbolO.png");
     private final Image symbolX = new Image("file:src/main/resources/symbolX.png");
 
+    public void displayBoardFieldsSymbols() {
+
+    }
+
+    public void clearBoardFields() {
+
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -32,14 +40,14 @@ public class TicTacToe extends Application {
         RowConstraints row3GF = new RowConstraints(114);
         gameField.getColumnConstraints().addAll(col1GF, col2GF, col3GF);
         gameField.getRowConstraints().addAll(row1GF, row2GF, row3GF);
-//        gameField.setGridLinesVisible(true);                //lines showing columns and rows
+//        gameField.setGridLinesVisible(true);                //lines showing columns and rows lines in application
 
         GridPane menuPanel = new GridPane();
         menuPanel.setPadding(new Insets(25, 25, 25, 0));
-        ColumnConstraints col0MP = new ColumnConstraints(110);              // in total 283 px
+        ColumnConstraints col0MP = new ColumnConstraints(110);    // columns in total 283 px
         ColumnConstraints col1MP = new ColumnConstraints(23);
         ColumnConstraints col2MP = new ColumnConstraints(150);
-        RowConstraints row0MP = new RowConstraints(30);                     // in total 342 px
+        RowConstraints row0MP = new RowConstraints(30);       // rows in total 342 px
         RowConstraints row1MP = new RowConstraints(50);
         RowConstraints row2MP = new RowConstraints(50);
         RowConstraints row3MP = new RowConstraints(78);
@@ -47,43 +55,43 @@ public class TicTacToe extends Application {
         RowConstraints row5MP = new RowConstraints(56);
         menuPanel.getColumnConstraints().addAll(col0MP, col1MP, col2MP);
         menuPanel.getRowConstraints().addAll(row0MP, row1MP, row2MP, row3MP, row4MP, row5MP);
-//        menuPanel.setGridLinesVisible(true);                //lines showing columns and rows
+//        menuPanel.setGridLinesVisible(true);                //lines showing columns and rows lines in application
 
         Button newGame = new Button("New Game");
-        menuPanel.add(newGame, 2,0);
+        menuPanel.add(newGame, 2, 0);
         GridPane.setHalignment(newGame, HPos.CENTER);
         GridPane.setValignment(newGame, VPos.TOP);
 
         Label player = new Label("Player");
-        menuPanel.add(player, 0,1);
+        menuPanel.add(player, 0, 1);
         player.setFont(new Font(24.0));
-        
+
         Label computer = new Label("Computer");
-        menuPanel.add(computer, 2,1);
+        menuPanel.add(computer, 2, 1);
         GridPane.setHalignment(computer, HPos.RIGHT);
         computer.setFont(new Font(24.0));
 
         Label vs = new Label("vs");
-        menuPanel.add(vs, 1,1);
+        menuPanel.add(vs, 1, 1);
         vs.setFont(new Font(24.0));
 
         Label turnOf = new Label("Turn of");
-        menuPanel.add(turnOf, 0,2, 1, 1);
+        menuPanel.add(turnOf, 0, 2, 1, 1);
         GridPane.setHalignment(turnOf, HPos.RIGHT);
         turnOf.setFont(new Font(24.0));
 
         Label statusLabel = new Label("Status:");
-        menuPanel.add(statusLabel, 0,3);
+        menuPanel.add(statusLabel, 0, 3);
         GridPane.setHalignment(statusLabel, HPos.RIGHT);
         statusLabel.setFont(new Font(24.0));
 
         Label statusDetailLabel = new Label("In Game");
-        menuPanel.add(statusDetailLabel, 2,3);
+        menuPanel.add(statusDetailLabel, 2, 3);
         GridPane.setHalignment(statusDetailLabel, HPos.LEFT);
         statusDetailLabel.setFont(new Font(24.0));
 
-        Label communicates = new Label(""); //You can't place\nyour symbol on\nenemy's field! & communicates.setUnderline(true);
-        menuPanel.add(communicates, 0,4, 3,2);
+        Label communicates = new Label(""); //You can't place your\nsymbol on enemy's field!\nPlease choose empty field. & communicates.setUnderline(true);
+        menuPanel.add(communicates, 0, 4, 3, 2);
         GridPane.setHalignment(communicates, HPos.CENTER);
         GridPane.setValignment(communicates, VPos.CENTER);
         communicates.setFont(new Font(24.0));
