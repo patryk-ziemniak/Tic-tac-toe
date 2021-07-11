41435709528727b82614b3e0ca64ae46ab88c988
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-07-11 20:44:31 +0200

    Created proper shape of UI

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -1,48 +1,102 @@
 package com.tictactoe;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
 
-    private Image background = new Image("file:src/main/resources/background.png");
-    private Image symbolO = new Image("file:src/main/resources/symbolO.png");
-    private Image symbolX = new Image("file:src/main/resources/symbolX.png");
+    private final Image background = new Image("file:src/main/resources/background.png");
+    private final Image symbolO = new Image("file:src/main/resources/symbolO.png");
+    private final Image symbolX = new Image("file:src/main/resources/symbolX.png");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        GridPane gameField = new GridPane();
+        gameField.setPadding(new Insets(25, 25, 25, 25));
+        ColumnConstraints col1GF = new ColumnConstraints(114);
+        ColumnConstraints col2GF = new ColumnConstraints(114);
+        ColumnConstraints col3GF = new ColumnConstraints(114);
+        RowConstraints row1GF = new RowConstraints(114);
+        RowConstraints row2GF = new RowConstraints(114);
+        RowConstraints row3GF = new RowConstraints(114);
+        gameField.getColumnConstraints().addAll(col1GF, col2GF, col3GF);
+        gameField.getRowConstraints().addAll(row1GF, row2GF, row3GF);
+//        gameField.setGridLinesVisible(true);                //lines showing columns and rows
+
+        GridPane menuPanel = new GridPane();
+        menuPanel.setPadding(new Insets(25, 25, 25, 0));
+        ColumnConstraints col0MP = new ColumnConstraints(110);              // in total 283 px
+        ColumnConstraints col1MP = new ColumnConstraints(23);
+        ColumnConstraints col2MP = new ColumnConstraints(150);
+        RowConstraints row0MP = new RowConstraints(30);                     // in total 342 px
+        RowConstraints row1MP = new RowConstraints(50);
+        RowConstraints row2MP = new RowConstraints(50);
+        RowConstraints row3MP = new RowConstraints(78);
+        RowConstraints row4MP = new RowConstraints(78);
+        RowConstraints row5MP = new RowConstraints(56);
+        menuPanel.getColumnConstraints().addAll(col0MP, col1MP, col2MP);
+        menuPanel.getRowConstraints().addAll(row0MP, row1MP, row2MP, row3MP, row4MP, row5MP);
+//        menuPanel.setGridLinesVisible(true);                //lines showing columns and rows
+
+        Button newGame = new Button("New Game");
+        menuPanel.add(newGame, 2,0);
+        GridPane.setHalignment(newGame, HPos.CENTER);
+        GridPane.setValignment(newGame, VPos.TOP);
+
+        Label player = new Label("Player");
+        menuPanel.add(player, 0,1);
+        player.setFont(new Font(24.0));
+        
+        Label computer = new Label("Computer");
+        menuPanel.add(computer, 2,1);
+        GridPane.setHalignment(computer, HPos.RIGHT);
+        computer.setFont(new Font(24.0));
+
+        Label vs = new Label("vs");
+        menuPanel.add(vs, 1,1);
+        vs.setFont(new Font(24.0));
+
+        Label turnOf = new Label("Turn of");
+        menuPanel.add(turnOf, 0,2, 1, 1);
+        GridPane.setHalignment(turnOf, HPos.RIGHT);
+        turnOf.setFont(new Font(24.0));
+
+        Label statusLabel = new Label("Status:");
+        menuPanel.add(statusLabel, 0,3);
+        GridPane.setHalignment(statusLabel, HPos.RIGHT);
+        statusLabel.setFont(new Font(24.0));
+
+        Label statusDetailLabel = new Label("In Game");
+        menuPanel.add(statusDetailLabel, 2,3);
+        GridPane.setHalignment(statusDetailLabel, HPos.LEFT);
+        statusDetailLabel.setFont(new Font(24.0));
+
+        Label communicates = new Label(""); //You can't place\nyour symbol on\nenemy's field! & communicates.setUnderline(true);
+        menuPanel.add(communicates, 0,4, 3,2);
+        GridPane.setHalignment(communicates, HPos.CENTER);
+        GridPane.setValignment(communicates, VPos.CENTER);
+        communicates.setFont(new Font(24.0));
+
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
         Background background = new Background(backgroundImage);
 
-        GridPane gridPane = new GridPane();
-        gridPane.setBackground(background);
-        gridPane.setPadding(new Insets(25, 25, 25, 25));
+        HBox root = new HBox();
+        root.setBackground(background);
+        root.getChildren().addAll(gameField, menuPanel);
 
-        ColumnConstraints col1 = new ColumnConstraints(114);
-        ColumnConstraints col2 = new ColumnConstraints(114);
-        ColumnConstraints col3 = new ColumnConstraints(114);
-        ColumnConstraints col4 = new ColumnConstraints(308);
-        RowConstraints row1 = new RowConstraints(114);
-        RowConstraints row2 = new RowConstraints(114);
-        RowConstraints row3 = new RowConstraints(114);
-        gridPane.getColumnConstraints().addAll(col1, col2, col3, col4);
-        gridPane.getRowConstraints().addAll(row1,row2,row3);
-        gridPane.setGridLinesVisible(true);                //lines showing columns and rows
-
-        ImageView Symbol11 = new ImageView(symbolO);
-        gridPane.add(Symbol11, 1, 1);
-
-        ImageView Symbol22 = new ImageView(symbolX);
-        gridPane.add(Symbol22, 2, 1);
-
-        Scene scene = new Scene(gridPane, 700, 392, Color.BLACK);
+        Scene scene = new Scene(root, 700, 392, Color.BLACK);
 
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(scene);
